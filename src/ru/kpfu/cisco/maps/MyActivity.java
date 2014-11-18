42c432ba292fd822d38a05a78e7978fdb4822adb
kas1906 <!=19061995alex=!>
2014-11-18 13:33:57 +0400

    initial commit

---
+++ src/ru/kpfu/cisco/maps/MyActivity.java
@@ -0,0 +1,32 @@
+package ru.kpfu.cisco.maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    private WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        webView = (WebView) findViewById(R.id.webView);
+        WebClient webClient = new WebClient();
+        webView.setWebViewClient(webClient);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webClient.shouldOverrideUrlLoading(webView, "file:///android_asset/index.html");
+    }
+
+
+}
